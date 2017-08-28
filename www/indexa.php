abf37bd776ca4391bd91b54c775ea992e6db02b5
Leandro Schwab Dias Carneiro <schwab_@hotmail.com>
2017-08-28 02:07:48 -0300

    start1
    preparando alguns arquivos

---
+++ www/indexa.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8"/>
+    <title>FarmaSIM</title>
+    <link rel="stylesheet" type="text/css" href="_css/estilo.css"/>
+</head>
+<body>
+<div id="interface">
+
+    <header id="cabecalho">
+
+        <ul>
+            <li><a href="pacientes.php">--Pacientes</a></li>
+            <li><a href="remedios.php"> --Remedios</a></li>
+            <li><a href="prep_receitas.php"> --Preparar receitas</a></li>
+            <li><a href="print_receitas.php"> --imprimir receitas</a></li>
+            <li><a href="sobre.php"> --Sobre</a></li>
+        </ul>
+
+    </header>
+
+
+</div>
+
+</body>
+
+</html>
