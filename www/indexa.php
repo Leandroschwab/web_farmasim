32f9b0ed71cfe9f30e5c300d1ca45ffeb68962ed
Leandro Schwab Dias Carneiro <schwab_@hotmail.com>
2017-08-28 14:11:05 -0300

    menu1
    adicionado algumas funcionalidades do menu

--- www/indexa.php
+++ www/indexa.php
@@ -3,21 +3,22 @@
 <head>
     <meta charset="UTF-8"/>
     <title>FarmaSIM</title>
-    <link rel="stylesheet" type="text/css" href="_css/estilo.css"/>
+    <link rel="stylesheet" href="_css/estilo.css"/>
 </head>
 <body>
 <div id="interface">
 
     <header id="cabecalho">
-
+<nav id="menu-top">
+    <title><h1>Menu principal</h1></title>
         <ul>
-            <li><a href="pacientes.php">--Pacientes</a></li>
-            <li><a href="remedios.php"> --Remedios</a></li>
-            <li><a href="prep_receitas.php"> --Preparar receitas</a></li>
-            <li><a href="print_receitas.php"> --imprimir receitas</a></li>
-            <li><a href="sobre.php"> --Sobre</a></li>
+            <li><a href="pacientes.php">Pacientes</a></li>
+            <li><a href="remedios.php">Remedios</a></li>
+            <li><a href="prep_receitas.php">Preparar receitas</a></li>
+            <li><a href="print_receitas.php">imprimir receitas</a></li>
+            <li><a href="sobre.php">Sobre</a></li>
         </ul>
-
+</nav>
     </header>
 
 
