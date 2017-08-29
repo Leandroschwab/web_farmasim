00cfaa77f58a8ea2257b452afa985a26e334f647
Leandro Schwab Dias Carneiro <schwab_@hotmail.com>
2017-08-29 17:43:31 -0300

    menu
    adicinado o footer e cor de fundo dinamica no menu

--- www/indexa.php
+++ www/indexa.php
@@ -22,6 +22,12 @@
     </header>
 
 
+    <footer id="rodape">
+
+<p>O presente trabalho foi realizado com o apoio do Programa Institucional de Bolsas de Iniciação à Inovação – PIBInova/PDI/UFF  da Agência de Inovação/PROPPI</p>
+
+    </footer>
+
 </div>
 
 </body>
